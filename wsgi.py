e7b73783702457e6047ea7067945dfce5ee7155a
Circuit Developers <55948776+circuitdevelopers-in@users.noreply.github.com>
2020-06-02 22:53:05 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,18 +3,38 @@
   
 #if __name__ == "__main__": 
 #        app.run() 
+from aiohttp import web
+import socketio
 
-from twisted.internet import protocol, reactor
-import os
-class Echo(protocol.Protocol):
-    def dataReceived(self, data):
-        print(data)
-        self.transport.write(data)
+# creates a new Async Socket IO Server
+sio = socketio.AsyncServer(cors_allowed_origins='*')
+# Creates a new Aiohttp Web Application
+app = web.Application()
+# Binds our Socket.IO server to our Web App
+# instance
+sio.attach(app)
 
-class EchoFactory(protocol.Factory):
-    def buildProtocol(self, addr):
-        return Echo()
+# we can define aiohttp endpoints just as we normally
+# would with no change
+async def index(request):
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
-port = int(os.environ.get('PORT', 8012))
-reactor.listenTCP(port, EchoFactory(), interface = '0.0.0.0')
-reactor.run()
+# If we wanted to create a new websocket endpoint,
+# use this decorator, passing in the name of the
+# event we wish to listen out for
+@sio.on('message')
+async def print_message(sid, message):
+    # When we receive a new event of type
+    # 'message' through a socket.io connection
+    # we print the socket ID and the message
+    print("Socket ID: " , sid)
+    print(message)
+
+# We bind our aiohttp endpoint to our app
+# router
+app.router.add_get('/', index)
+
+# We kick off our server
+if __name__ == '__main__':
+    web.run_app(app)
