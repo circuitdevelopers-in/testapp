9412516b2462840c3a7ae063d4d64028880e785a
Circuit Developers <55948776+circuitdevelopers-in@users.noreply.github.com>
2020-06-02 22:07:59 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,19 @@
 
-from app.main import app 
+#from app.main import app 
   
-if __name__ == "__main__": 
-        app.run() 
+#if __name__ == "__main__": 
+#        app.run() 
 
+from twisted.internet import protocol, reactor
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        print(data)
+        self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+
+reactor.listenTCP(8080, EchoFactory())
+reactor.run()
